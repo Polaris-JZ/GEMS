f73a103f64cc183e730f1ea0711b7e908eaca779
Jujia Zhao <zhao.jujia.0913@gmail.com>
2026-08-29 15:20:10 +0800

    Remove obsolete files from previous release

--- subspace_torch/__init__.py
+++
@@ -1,4 +0,0 @@
-from .adafactor import Adafactor as GaLoreAdafactor
-from .adamw import AdamW as GaLoreAdamW
-from .adamw8bit import AdamW8bit as GaLoreAdamW8bit
-from .adamw import MultiTaskGaLoreAdamW as MultiTaskGaLoreAdamW
